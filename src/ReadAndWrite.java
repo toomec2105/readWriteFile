c475db50be27a3917a5bc53ecdf10c124d2ada6e
toomec2105 <tomec2105@interia.pl>
2020-04-22 16:55:48 +0200

    bufferedReader

--- src/ReadAndWrite.java
+++ src/ReadAndWrite.java
@@ -1,9 +1,10 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
-
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 
@@ -13,27 +14,14 @@ public class ReadAndWrite {
 
 		String path = "/home/darek/eclipse-workspace/Read&WriteFromFile/sourceFile";
 
-		String textFromFile = "";
-// How to add new line to a string
-// Extract the first pat into method readFileWithScanner
-// Replace the method with BufferedReader and FileReader
 		
-		try {
-			File file = new File(path);
-			Scanner scanner = new Scanner(file);
-
-			while (scanner.hasNextLine()) {
-				textFromFile += scanner.nextLine();
-				// System.out.println(textFromFile);
-			}
-
-			scanner.close();
-
-		} catch (FileNotFoundException e) {
-			System.out.println("An error occurred when trying to read a file.");
-			e.printStackTrace();
-		}
-
+// How to add new line to a string---- done
+// Extract the first part into method readFileWithScanner----done
+// Replace the method with BufferedReader and FileReader----done
+		
+		 String textFromFile = "";
+		textFromFile = readFileWithBufferedReader();
+		//textFromFile = readFileWithScanner(path);
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter("targetFile"));
 			writer.write("This is written text" + textFromFile);
@@ -44,5 +32,43 @@ public class ReadAndWrite {
 			e.printStackTrace();
 			
 		}
+	}
+	public static String readFileWithBufferedReader() {
+		String textFromFile = ""; 
+		try {
+				BufferedReader reader = new BufferedReader(new FileReader("sourceFile"));
+				String line;
+
+				while( (line = reader.readLine() ) != null) {
+				textFromFile += line + "\n";
+				}
+				reader.close();
+
+			} catch (IOException e) {
+				System.out.println("An error occurred when trying to write to a file.");
+				e.printStackTrace();
+				
+			}
+		return textFromFile;
+	}
+	
+	public static String readFileWithScanner(String path) {
+		String textFromFile = "";
+		try {
+			
+			File file = new File(path);
+			Scanner scanner = new Scanner(file);
+
+			while (scanner.hasNextLine()) {
+				textFromFile += scanner.nextLine() + "\n";
+				// System.out.println(textFromFile);
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred when trying to read a file.");
+			e.printStackTrace();
+		}
+		return textFromFile;
+		
 	}
 }
