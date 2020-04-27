21d5cc9854eb744f6317691ad1f4653d622b8170
toomec2105 <tomec2105@interia.pl>
2020-04-27 19:24:36 +0200

    minor changes in ReadAndWrite.java

--- src/ReadAndWrite.java
+++ src/ReadAndWrite.java
@@ -11,6 +11,7 @@ import java.io.FileWriter;
 public class ReadAndWrite {
 
 	public static void main(String[] args) {
+		// some comment
 
 		String path = "/home/darek/eclipse-workspace/Read&WriteFromFile/sourceFile";
 
