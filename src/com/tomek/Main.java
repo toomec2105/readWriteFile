940d4622e82dce3fb0ed1f26548b446c100a6c0b
toomec2105 <tomec2105@interia.pl>
2020-05-01 13:41:57 +0200

    minor changes

--- src/com/tomek/Main.java
+++ src/com/tomek/Main.java
@@ -9,21 +9,22 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-	
+
 	// fields
-	 private static String path = "/home/darek/eclipse-workspace/Read&WriteFromFile/sourceFile";
-	 private static String targetFile = "/home/darek/eclipse-workspace/Read&WriteFromFile/targetFile";
+	private static String path = "/home/darek/eclipse-workspace/Read&WriteFromFile/sourceFile";
+	private static String targetFile = "/home/darek/eclipse-workspace/Read&WriteFromFile/targetFile";
+
 	public static void main(String[] args) {
-		
-		// creating an array of interface type which will store objects of classes implementing this interface 
-		
-		WriteProcessor[] writers =  new WriteProcessor[2];  
+
+		//------------- creating an array of interface type which will store objects of classes implementing this interface ------
+
+		WriteProcessor[] writers = new WriteProcessor[2];
 		writers[0] = new NIOFilesWriter();
 		writers[1] = new BufferedWriterWriter();
-		
+
 		for (int i = 0; i < writers.length; i++) {
-			String pathAsString = "arrayTest" + i ;
-			String text = writers[i].getClass().getSimpleName(); 
+			String pathAsString = "arrayTest" + i;
+			String text = writers[i].getClass().getSimpleName();
 			try {
 				OutputStream out = Files.newOutputStream(Paths.get(pathAsString));
 				writers[i].write(pathAsString, text);
@@ -31,15 +32,16 @@ public class Main {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
-		
-		//WriteProcessor writer1 = new WriteProcessor(); Cannot instantiate the type WriteProcessor = i can't create an object from an interface
-				//[new BufferedWriterWriter()];
-				
-		
-		//------------ calls to generic readFromAndWriteToFile_GENERIC ----------------------
-		
+
+		// WriteProcessor writer1 = new WriteProcessor(); Cannot instantiate the type
+		// WriteProcessor = i can't create an object from an interface
+		// [new BufferedWriterWriter()];
+
+		// ------------ calls to generic readFromAndWriteToFile_GENERIC
+		// ----------------------
+
 		/*
 		 * readFromAndWriteToFile_GENERIC(path, targetFile, new BufferedReaderReader(),
 		 * new NIOFilesWriter()); readFromAndWriteToFile_GENERIC(targetFile, targetFile,
@@ -49,56 +51,53 @@ public class Main {
 		 * new ScannerReader(), new BufferedWriterWriter());
 		 * 
 		 */
-		
-	
-		
-		
-		//------------ calls to generic readFromAndWriteToFile_GENERIC using anonymous classes----------------------
-		
+
+		// ------------ calls to generic readFromAndWriteToFile_GENERIC using anonymous
+		// classes----------------------
+
 		ReadProcessor anonymousClassImplementingReadProcessor = new ReadProcessor() {
 			@Override
 			public String read(String path) {
-				String textFromFile = ""; 
+				String textFromFile = "";
 				try {
-						BufferedReader reader = new BufferedReader(new FileReader(path));
-						String line;
+					BufferedReader reader = new BufferedReader(new FileReader(path));
+					String line;
 
-						while( (line = reader.readLine() ) != null) {
+					while ((line = reader.readLine()) != null) {
 						textFromFile += line + "\n";
-						}
-						reader.close();
-
-					} catch (IOException e) {
-						System.out.println("An error occurred when trying to write to a file.");
-						e.printStackTrace();
-						
 					}
+					reader.close();
+
+				} catch (IOException e) {
+					System.out.println("An error occurred when trying to write to a file.");
+					e.printStackTrace();
+
+				}
 				return textFromFile;
 			}
 		};
-		
-	
+
 		WriteProcessor anonymousClassImplementingWriteProcessor = new WriteProcessor() {
-			
+
 			@Override
 			public void write(String targetFile, String text) {
 				try {
-		        	Path pathToTargetFile = Paths.get(targetFile); // old java.io has similar class File(String pathToFile)	        	
-		        	byte[] textAsBytes = text.getBytes();
-		            Files.write(pathToTargetFile, textAsBytes);
-		            
-		        } catch (IOException e) {
-		            e.printStackTrace();
-		        }				
+					Path pathToTargetFile = Paths.get(targetFile); // old java.io has similar class File(String
+																	// pathToFile)
+					byte[] textAsBytes = text.getBytes();
+					Files.write(pathToTargetFile, textAsBytes);
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		};
-		
+
 		String textFromFile = anonymousClassImplementingReadProcessor.read(path);
 		anonymousClassImplementingWriteProcessor.write(targetFile, textFromFile);
-		
-		
-		//------------ calls to non-generic readFromAndWriteToFile ----------------------
-		
+
+		// ------------ calls to non-generic readFromAndWriteToFile
+		// ----------------------
 
 		/*
 		 * readFromAndWriteToFile(path, targetFile, new BufferedReaderReader(), new
@@ -109,42 +108,46 @@ public class Main {
 		 * ScannerReader(), new BufferedWriterWriter());
 		 */
 	}
-	
-	
-	
-	//------------generic readFromAndWriteToFile ----------------------
 
-	
-	public static void readFromAndWriteToFile_GENERIC(String sourceFile, String targetFile, ReadProcessor reader, WriteProcessor writer) {
+	// ------------generic readFromAndWriteToFile ----------------------
+
+	public static void readFromAndWriteToFile_GENERIC(String sourceFile, String targetFile, ReadProcessor reader,
+			WriteProcessor writer) {
 		System.out.println("Calling a generic method");
 		String text = reader.read(sourceFile);
-		text += " " + reader.getClass().getSimpleName() + " with " + writer.getClass().getSimpleName(); 
-		writer.write(targetFile, text);		
+		text += " " + reader.getClass().getSimpleName() + " with " + writer.getClass().getSimpleName();
+		writer.write(targetFile, text);
 	}
-	
-	//------------ non-generic readFromAndWriteToFile ----------------------
-	
-	
-	public static void readFromAndWriteToFile(String sourceFile, String targetFile, BufferedReaderReader reader, NIOFilesWriter writer) {
+
+	// ------------ non-generic readFromAndWriteToFile ----------------------
+
+	public static void readFromAndWriteToFile(String sourceFile, String targetFile, BufferedReaderReader reader,
+			NIOFilesWriter writer) {
 		System.out.println("Calling a non-generic method");
 
 		String text = reader.read(sourceFile);
-		text += " " + reader.getClass().getSimpleName() + " with " + writer.getClass().getSimpleName(); 
-		writer.write(targetFile, text);		
-	}
-	public static void readFromAndWriteToFile(String sourceFile, String targetFile, ScannerReader reader, BufferedWriterWriter writer) {
-		String text = reader.read(sourceFile);
-		text += " " + reader.getClass().getSimpleName() + " with " + writer.getClass().getSimpleName(); 
+		text += " " + reader.getClass().getSimpleName() + " with " + writer.getClass().getSimpleName();
 		writer.write(targetFile, text);
 	}
-	public static void readFromAndWriteToFile(String sourceFile, String targetFile, ScannerReader reader, NIOFilesWriter writer) {
+
+	public static void readFromAndWriteToFile(String sourceFile, String targetFile, ScannerReader reader,
+			BufferedWriterWriter writer) {
 		String text = reader.read(sourceFile);
-		text += " " + reader.getClass().getSimpleName() + " with " + writer.getClass().getSimpleName(); 
+		text += " " + reader.getClass().getSimpleName() + " with " + writer.getClass().getSimpleName();
 		writer.write(targetFile, text);
 	}
-	public static void readFromAndWriteToFile(String sourceFile, String targetFile, BufferedReaderReader reader, BufferedWriterWriter writer) {
+
+	public static void readFromAndWriteToFile(String sourceFile, String targetFile, ScannerReader reader,
+			NIOFilesWriter writer) {
 		String text = reader.read(sourceFile);
-		text += " " + reader.getClass().getSimpleName() + " with " + writer.getClass().getSimpleName(); 
+		text += " " + reader.getClass().getSimpleName() + " with " + writer.getClass().getSimpleName();
+		writer.write(targetFile, text);
+	}
+
+	public static void readFromAndWriteToFile(String sourceFile, String targetFile, BufferedReaderReader reader,
+			BufferedWriterWriter writer) {
+		String text = reader.read(sourceFile);
+		text += " " + reader.getClass().getSimpleName() + " with " + writer.getClass().getSimpleName();
 		writer.write(targetFile, text);
 	}
 }
